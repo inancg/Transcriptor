fea983a5dd56d3bd851a60c3981a078464f85944
inang <inanc.gurkan@yahoo.com>
2021-03-29 21:16:53 +0100

    watson response tests and placeholder jsons

---
+++ tests/watson/test_watson_response_handler.py
@@ -0,0 +1,33 @@
+import unittest
+
+from src.watson.response_handler import ResponseHandler \
+    as WatsonResponseHandler
+from tests.common.constants import WATSON_TEST_RESOURCES_LOCATION, \
+    TEST_CONFIDENCE_THRESHOLD, TRANSCRIBE_ITEMS_SUSPICIOUS, \
+    TRANSCRIBE_ITEMS_NOT_SUSPICIOUS
+
+
+class TestWatsonResponseHandler(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.response_handler = WatsonResponseHandler(confidence_threshold=
+                                                     TEST_CONFIDENCE_THRESHOLD)
+
+    def test_extract_results_with_suspicion(self):
+        self.assertDictEqual(
+            self.response_handler.extract_results(
+                json_file=WATSON_TEST_RESOURCES_LOCATION + 'suspicious.json'),
+            TRANSCRIBE_ITEMS_SUSPICIOUS
+        )
+
+    def test_extract_results_without_suspicion(self):
+        self.assertDictEqual(
+            self.response_handler.extract_results(
+                json_file=WATSON_TEST_RESOURCES_LOCATION + 'not_suspicious'
+                                                           '.json'),
+            TRANSCRIBE_ITEMS_NOT_SUSPICIOUS
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
